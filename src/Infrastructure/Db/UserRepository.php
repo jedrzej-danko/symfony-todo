1085bb208ec07676f3c68c5670bcbc1c520a4468
jedrzej <jedrzej.danko@gmail.com>
2023-03-15 17:00:31 +0100

    Maintain phpstan level 9 compliance

--- src/Infrastructure/Db/UserRepository.php
+++ src/Infrastructure/Db/UserRepository.php
@@ -57,7 +57,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 
         $user->setPassword($newHashedPassword);
 
-        $this->save($user, true);
+        $this->save($user);
     }
 
 //    /**
